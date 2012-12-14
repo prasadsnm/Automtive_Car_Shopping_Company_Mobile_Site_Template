47700bba9f739f38d4d89e80fb87c99043824f34
Kaidul Islam <ikaidul@yahoo.com>
2012-12-14 06:24:44 -0800

    Update exhibit.php
    Fixing the facebook and twitter share option

--- exhibit.php
+++ exhibit.php
@@ -1,10 +1,92 @@
 <? include("header.php"); 
    $id = isset($_GET['id'])?$_GET['id']:"";
    $jsid = json_encode($id);   
-?>
+?>       
+<div data-role="content" data-theme="b">
+  <div id="car_heading"></div>
+  <div>
+  <a href="tel:800-718-1720" class="button2" data-role="button" data-icon="phonn" data-theme="a" style="color:#000;">
+    Call Now!
+  </a>
+  <a href="" class="button2" data-role="button" data-icon="fav" data-theme="a" style="color:#000;" id="fav_1">
+    Add To Favorites
+  </a>
+  <a href="" class="button2" data-role="button" data-icon="fav" data-theme="a" style="color:#000;display:none;" id="fav_2">
+    Remove From Favorites
+  </a>  
+  <a href="" class="button2" data-role="button" data-icon="photos" data-theme="a" style="color:#000;" id="photos">
+    View All Photos
+  </a>    
+  <a href="" class="button2" data-role="button" data-icon="photos" data-theme="a" style="color:#000; display:none;" 
+
+id="lessphotos">
+    View Less Photos
+  </a>  
+  </div>
+
+  <div id="exhibition"><img src=""/> </div>
+  <div id="price_bar">
+    <span>Price:</span> 
+    <span id="r"></span>
+  </div>
+  <div id="friend">
+    Ask your friends if you should buy this car!
+  </div>
+
+    <div id='fb-root'></div>
+    <script src='http://connect.facebook.net/en_US/all.js'></script>
+    <div>
+  <a onclick='postToFeed(); return false;' class="button2" data-role="button" data-icon="fb" data-theme="a" 
+
+style="color:#3B5998;">
+    Ask Facebook Friends!
+  </a>
+  <p id='msg'></p>
+  <a href="" id="button2"  data-count="none" data-role="button" data-icon="twitt" data-theme="a" style="color:#000;">
+    Ask Twitter Followers!
+  </a>
+  <script>
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+  </script> 
+  <a href="loan_form.php" class="button2" data-role="button" data-icon="mail" data-theme="a" style="color:#000;">
+    Meassage Us!
+  </a>
+  <a href="loan_form.php" class="button2" data-role="button" data-icon="loaner" data-theme="a" style="color:#000;">
+    Need Financing?
+  </a>  
+  <a href="trade_form.php" class="button2" data-role="button" data-icon="trader" data-theme="a" style="color:#000;">
+    Trade-In for this Car!
+  </a>  
+    </div>
+      <div id="friend">
+       Car Details
+      </div>
+    <div>
+        <table id="cardetails">
+          <tbody>
+             <?php
+               $itemArray = array("Make", "Model", "Year", "Vin Number", "Stock Number", "Body Style", "Ext. Color", 
+
+"Int. Color", "Trim", "Drive Train", "Trans. Type", "Eng. Displacement");
+               for ($i=0; $i < sizeof($itemArray); $i++) { 
+                ?>
+                <tr>
+                    <td id="right"><?=$itemArray[$i].": "?></td> 
+                    <td id="left"></td>
+                  </tr>                    
+                <?
+               }
+             ?>     
+          </tbody>
+        </table>
+    </div><br/><br/>
+    <div id="desc"></div>
+</div>
 <script type="text/javascript">
-      $(document).ready(function(){
+      var imageUrl, title, url;
+  $(document).ready(function(){
         <? echo "var ID = $jsid;" ?>
+        url = "http://mypushportal.com/demo/exhibit.php?id=" + ID;
         var vehicleUrl = "http://multichoice.dealercp.com/vehicle/?id=" + ID + "&format=json&jsoncallback=?";
         function addCommas(str) {
              var amount = new String(str);
@@ -16,38 +98,26 @@
              }
              return output;
         }        
-        var imageUrl, title;
 
 
         $.getJSON(vehicleUrl, function(item){
-                	$('#car_heading').html(item.year + ' ' + item.make + ' ' + item.model); 
+                  $('#car_heading').html(item.year + ' ' + item.make + ' ' + item.model); 
                   $('#desc').html(item.desc); 
-                  title = 'Should I buy this car ' + item.year + ' ' + item.make + ' ' + item.model;
-                	$('#exhibition img').attr('src', item.pic[0].full);
+                  title = 'Should I buy this car? - ' + item.year + ' ' + item.make + ' ' + item.model + ' ('+ item.color.ext +') - available now at MultiChoiceApps Car Pursuit, GA';
+
+                  var twittUrl = "https://twitter.com/share?text=" + title + "&hashtag=carpursuit";
+                  $('#button2').attr('href', twittUrl);
+
+                  $('#exhibition img').attr('src', item.pic[0].full);
                   imageUrl = item.pic[0].full;
-                	$('#price_bar #r').html('$' + addCommas(item.price.net));
-                	var Details = new Array(item.make, item.model, item.year, item.vin, item.stock, item.body, item.color.ext, item.color.int, item.trim, item.power.drive, item.power.trans, item.power.eng);
-                	var tr = $("#cardetails tr");
-                	tr.each(function(){
-                		$(this).children('td:nth-child(2)').html(Details[$(this).index()]);
-                	});
+                  $('#price_bar #r').html('$' + addCommas(item.price.net));
+                  var Details = new Array(item.make, item.model, item.year, item.vin, item.stock, item.body, item.color.ext, item.color.int, item.trim, item.power.drive, item.power.trans, item.power.eng);
+                  var tr = $("#cardetails tr");
+                  tr.each(function(){
+                    $(this).children('td:nth-child(2)').html(Details[$(this).index()]);
+                  });
  
         });
- /*
-(function($) {
-    $.fn.clickToggle = function(func1, func2) {
-        var funcs = [func1, func2];
-        this.data('toggleclicked', 0);
-        this.click(function() {
-            var data = $(this).data();
-            var tc = data.toggleclicked;
-            $.proxy(funcs[tc], this)();
-            data.toggleclicked = (tc + 1) % 2;
-        });
-        return this;
-    };
-}(jQuery));
-*/
         $('#photos').click(function(){
           $.getJSON(vehicleUrl, function(item){
             $('#exhibition').html("");
@@ -85,100 +155,23 @@
 
 
       });
-</script>
-<div id='fb-root'></div>
-    <script src='http://connect.facebook.net/en_US/all.js'></script>
-    <script> 
-    /*
-      FB.init({appId: "YOUR_APP_ID", status: true, cookie: true});
-
+      //facebook API
+      FB.init({appId: '134384696717251', status: true, cookie: true});
       function postToFeed() {
-
-        // calling the API ...
         var obj = {
           method: 'feed',
-          redirect_uri: 'https://multichoiceapps.com/demo/',
-          link: 'https://developers.facebook.com/docs/reference/dialogs/',
+          link: url,
           picture: imageUrl,
-          name: 'Facebook Dialogs',
-          caption: 'Reference Documentation',
-          description: 'Using Dialogs to interact with users.'
+          name: title,
+          caption: 'MultiChoiceApps Car Inventory Apps',
+          description: 'Car pursuit, GA is a mobile apps developed by a leading marketing company,MultichoiceApps from Atlanta.The developer is Kaidul from Bangladesh.'
         };
 
-        // function callback(response) {
-        //   document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
-        // }
+         function callback(response) {
+           document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
+         }
 
         FB.ui(obj);
       }
-      */
-    
-    </script>    
-<div data-role="content" data-theme="b">
-	<div id="car_heading"></div>
-	<div>
-	<a href="tel:800-718-1720" class="button2" data-role="button" data-icon="phonn" data-theme="a" style="color:#000;">
-		Call Now!
-	</a>
-	<a href="" class="button2" data-role="button" data-icon="fav" data-theme="a" style="color:#000;" id="fav_1">
-		Add To Favorites
-	</a>
-  <a href="" class="button2" data-role="button" data-icon="fav" data-theme="a" style="color:#000;display:none;" id="fav_2">
-    Remove From Favorites
-  </a>  
-	<a href="" class="button2" data-role="button" data-icon="photos" data-theme="a" style="color:#000;" id="photos">
-		View All Photos
-	</a>		
-  <a href="" class="button2" data-role="button" data-icon="photos" data-theme="a" style="color:#000; display:none;" id="lessphotos">
-    View Less Photos
-  </a>  
-	</div>
-
-	<div id="exhibition"><img src=""/> </div>
-	<div id="price_bar">
-		<span>Price:</span> 
-		<span id="r"></span>
-	</div>
-	<div id="friend">
-		Ask your friends if you should buy this car!
-	</div>
-    <div>
-	<a onclick='postToFeed(); return false;' class="button2" data-role="button" data-icon="fb" data-theme="a" style="color:#3B5998;">
-		Ask Facebook Friends!
-	</a>
-	<a href="" class="button2" data-role="button" data-icon="twitt" data-theme="a" style="color:#000;">
-		Ask Twitter Followers!
-	</a>	
-	<a href="loan_form.php" class="button2" data-role="button" data-icon="mail" data-theme="a" style="color:#000;">
-		Meassage Us!
-	</a>
-	<a href="loan_form.php" class="button2" data-role="button" data-icon="loaner" data-theme="a" style="color:#000;">
-		Need Financing?
-	</a>	
-	<a href="trade_form.php" class="button2" data-role="button" data-icon="trader" data-theme="a" style="color:#000;">
-		Trade-In for this Car!
-	</a>	
-    </div>
-    	<div id="friend">
-		   Car Details
-	    </div>
-    <div>
-        <table id="cardetails">
-        	<tbody>
-        	   <?php
-        	     $itemArray = array("Make", "Model", "Year", "Vin Number", "Stock Number", "Body Style", "Ext. Color", "Int. Color", "Trim", "Drive Train", "Trans. Type", "Eng. Displacement");
-        	     for ($i=0; $i < sizeof($itemArray); $i++) { 
-        	     	?>
-    		        <tr>
-        		        <td id="right"><?=$itemArray[$i].": "?></td> 
-        		        <td id="left"></td>
-        	        </tr>         	     	   
-        	     	<?
-        	     }
-        	   ?>  		
-        	</tbody>
-        </table>
-    </div><br/><br/>
-    <div id="desc"></div>
-</div>
+    </script> 
 <? include("footer.php"); ?>
